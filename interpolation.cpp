c86eac82d8e700067eea89d0912370a059ca90fe
fusheng-ji <1634442468@qq.com>
2022-07-10 17:47:24 +0800

    change some error

--- interpolation.cpp
+++ interpolation.cpp
@@ -9,6 +9,6 @@ torch::Tensor trilinear_interpolation(
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
 	m.def("trilinear_interpolation", &trilinear_interpolation, R"pbdoc(
-		m.def("name_in_python",&name_in_c++)
+		m.def("name_in_python",&name_in_cpp)
 		)pbdoc");
 }
